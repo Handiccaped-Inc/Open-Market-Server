d942cb91306f7dafafc376ac6b53e904ba10041f
Jojan-Esteban-Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-05-14 00:56:48 -0500

    Removed the problematic things

---
+++ src/main/java/co/unicauca/openmarket/server/access/ICategoryRepository.java
@@ -0,0 +1,10 @@
+package co.unicauca.openmarket.server.access;
+
+import co.unicauca.openmarket.commons.domain.Category;
+
+/**
+ *
+ * @author Libardo, Julio
+ */
+public interface ICategoryRepository extends IRepository<Category> {
+}
