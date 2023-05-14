84dd003e31b7e0f8bba4c119e220adfd628c7b66
SantiagoAV <81932949+SantiAV25@users.noreply.github.com>
2023-05-14 00:04:08 -0500

    try firxServer

--- Open-Marke-Server/src/main/java/co/unicauca/openmarket/server/access/IRepository.java
+++ Open-Marke-Server/src/main/java/co/unicauca/openmarket/server/access/IRepository.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.edu.unicauca.openmarket.access;
+package co.unicauca.openmarket.access;
 
 import java.util.List;
 
